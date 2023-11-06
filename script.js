7497d605db1f782c2d347d7229899581186b1ad8
123horlorlade <150072324+123horlorlade@users.noreply.github.com>
2023-11-06 15:15:39 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+const navBar = document.getElementById('navBar')
+
+
+function opentab(){
+  navBar.style.display = 'block';
+}
+
+function closetab(){
+  navBar.style.display = 'none';
+}
+
+// login list 
+
+
+
+function selectFied(){
+  document.getElementById("list").style.display = "block";
+}
